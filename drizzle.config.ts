b61b5bfd08694b743ac96f7fe68aab96a9a9a016
musagenius345 <musabadru@gmail.com>
2024-02-04 13:47:35 +0300

    fix: drizzle db path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,14 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
-import { dbPath } from './test';
+// import { dbPath } from './test';
 
 export default {
   schema: './db/schema.ts',
   out: './drizzle',
   driver: 'better-sqlite',
   dbCredentials: {
-    url: dbPath ?? '/users.db'
+    url: './users.db'
     },
 } satisfies Config;
 
 
-console.log(dbPath, 'in drizzle.config.ts');
